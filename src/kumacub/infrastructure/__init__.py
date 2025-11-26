d5e624c036798e1eed19233427e142f918917d41
Steve Jibson <steve@jibson.com>
2025-11-26 06:55:41 -0700

    Update copyright text

--- src/kumacub/infrastructure/__init__.py
+++ src/kumacub/infrastructure/__init__.py
@@ -1,5 +1,5 @@
 #  KumaCub - Run local checks; push results to Uptime Kuma.
-#  Copyright (c) 2025.
+#  Copyright (c) 2025 Stephen T. Jibson.
 #  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
 #  License as published by the Free Software Foundation, version 3.
 #  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
